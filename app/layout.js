a17a6a96609742aea8f20ab8bf543cb5ba4e3352
meghawarna7 <meghzpro@gmail.com>
2024-11-18 16:27:53 +0530

    basic structure

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -23,8 +25,11 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Header/>
         {children}
+        <Footer/>
       </body>
+     
     </html>
   );
 }
